393c4942e16387d969850f5bf836b3a37dc199a4
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-06-13 15:03:20 +0800

    hook up sanity with the frontend and create necessary components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
+import { Product, FooterBanner, HeroBanner } from "../components";
+import { client } from "../lib/client";
+const Home = ({ products, bannerData }) => {
   return (
-    <div className={styles.container}>
-      Hello
+    <div>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
+      <div className="products-heading">
+        <h2>Best Selling product</h2>
+        <p>Speakers of many variations </p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
+      <FooterBanner />
     </div>
-  )
-}
+  );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;

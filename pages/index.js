19d380139709abb7825cd6ed9a5ed351df6ef815
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-06-14 16:12:07 +0800

    StateContext added and also quantity increase and decrease implemented on a single product

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,17 @@ const Home = ({ products, bannerData }) => {
   return (
     <div>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      {console.log(bannerData)}
+      {console.log(products)}
       <div className="products-heading">
         <h2>Best Selling product</h2>
         <p>Speakers of many variations </p>
       </div>
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </div>
   );
 };
